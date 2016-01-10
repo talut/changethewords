15e22c2a8a0256b6a0b05ec5e007e6e05f74d0d7
ataluttasgiran <taluttasgiran@gmail.com>
2016-01-10 15:11:00 +0200

    ChangeTheWordsFirst

---
+++ jquery.changethewords.js
@@ -0,0 +1,29 @@
+(function($) {
+  $.fn.changeWords = function(options) {
+    var settings = $.extend({
+      time: 1500,
+      animate: "zoomIn",
+      selector: "span"
+    }, options);
+    var wordCount = $(settings.selector, this).size();
+    var words = $(settings.selector, this);
+    words.filter(function() {
+      return $(this).attr("data-id") != "1"
+    }).css("display", "none");
+    var count = 1;
+    setInterval(function() {
+      ++count;
+      var wordOrder = count;
+      words.filter(function() {
+        return $(this).attr("data-id") == wordOrder
+      }).addClass("animated " + settings.animate).css("display", "inline-block");
+      words.filter(function() {
+        return $(this).attr("data-id") != wordOrder
+      }).css("display", "none").removeClass();
+      if (count == wordCount) {
+        count = 0;
+      }
+    }, settings.time);
+    alert(order);
+  }
+}(jQuery));
